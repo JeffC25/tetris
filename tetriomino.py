e4f3333626f3b6d98c7e6cbf6b0aeedc8846af03
JeffC25 <jefflichen25@gmail.com>
2023-04-12 14:51:39 -0400

    tetromino class

---
+++ tetriomino.py
@@ -0,0 +1,11 @@
+from settings import *
+
+class Block:
+    pass
+
+class Tetromino:
+    def __init__(self, tetris):
+        pass
+    
+    def update(self):
+        pass
--- tetris.py
+++ tetris.py
@@ -1,9 +1,11 @@
 from settings import *
 import math
+from tetromino import Tetromino
 
 class Tetris:
     def __init__(self, app):
         self.app = app
+        self.tetromino = Tetromino(self)
 
     def drawGrid(self):
         for i in range(FIELD_W):
@@ -12,7 +14,7 @@ class Tetris:
                     (i * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE), 1)
         
     def update(self):
-        pass
+        self.tetromino.update()
 
     def draw(self):
-        pass
+        self.drawGrid()
